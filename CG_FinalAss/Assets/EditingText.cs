9f562b3eb6117fe0299d42d2fb985a66ed5200a5
benokelvin <kelvinlee981@outlook.com>
2020-09-27 04:58:55 +0800

    Profile Page

---
+++ CG_FinalAss/Assets/EditingText.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EditingText : MonoBehaviour
+{
+    GameObject button;
+    public GameObject panel;
+    public InputField inputText;
+    Text outputText;
+    public Text category;
+    string textcontent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textcontent = inputText.text;
+    }
+
+    public void openPanel()
+    {
+        button = EventSystem.current.currentSelectedGameObject;
+        panel.SetActive(true);
+        outputText = button.gameObject.GetComponentInChildren<Text>();
+        Debug.Log(outputText.name);
+        category.text = outputText.name.ToString();
+    }
+
+    public void saveChanges()
+    {
+        outputText.text = textcontent;
+        panel.SetActive(false);
+    }
+
+    public void discardChanges()
+    {
+        panel.SetActive(false);
+    }
+
+}
---
+++ CG_FinalAss/Assets/Navigation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour
+{
+    public GameObject biography, biographyPanel;
+    public GameObject skills, skillsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        biographyPanel.SetActive(false);
+        skillsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenBiography()
+    {
+        biography.SetActive(false);
+        skills.SetActive(false);
+        biographyPanel.SetActive(true);
+    }
+
+    public void CloseBiography()
+    {
+        biography.SetActive(true);
+        skills.SetActive(true);
+        biographyPanel.SetActive(false);
+    }
+
+    public void OpenSkills()
+    {
+        biography.SetActive(false);
+        skills.SetActive(false);
+        skillsPanel.SetActive(true);
+    }
+
+    public void CloseSkills()
+    {
+        biography.SetActive(true);
+        skills.SetActive(true);
+        skillsPanel.SetActive(false);
+    }
+}
