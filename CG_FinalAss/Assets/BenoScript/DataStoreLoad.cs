54dd019eeb18622f85dda4f82dfda67fc6ab051c
benokelvin <kelvinlee981@outlook.com>
2020-09-28 04:18:17 +0800

    Update Save and Load

--- CG_FinalAss/Assets/BenoScript/DataStoreLoad.cs
+++ CG_FinalAss/Assets/BenoScript/DataStoreLoad.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DataStoreLoad : MonoBehaviour
 {
@@ -30,6 +31,33 @@ public class DataStoreLoad : MonoBehaviour
     public void SaveFile()
     {
         SaveLoad.SaveData(this);
+        SceneManager.LoadScene("MainMenu");
     }
 
+    public void LoadFile()
+    {
+        SceneManager.LoadScene("ProfileEdit");
+        ProfileData data = SaveLoad.LoadData();
+
+        profileText[0].text = data.name;
+        profileText[1].text = data.introduction;
+        profileText[2].text = data.phone;
+        profileText[3].text = data.address;
+        profileText[4].text = data.email;
+
+        biographyText[0].text = data.age;
+        biographyText[1].text = data.IC;
+        biographyText[2].text = data.DOB;
+        biographyText[3].text = data.gender;
+        biographyText[4].text = data.marital;
+        biographyText[5].text = data.nationality;
+        biographyText[6].text = data.language;
+        biographyText[7].text = data.education;
+        biographyText[8].text = data.strength;
+        biographyText[9].text = data.objective;
+        biographyText[10].text = data.hobby;
+
+        experience.text = data.experience;
+        skills.text = data.skills;
+    }
 }
--- CG_FinalAss/Assets/BenoScript/SaveLoad.cs
+++ CG_FinalAss/Assets/BenoScript/SaveLoad.cs
@@ -7,7 +7,7 @@ public static class SaveLoad
     public static void SaveData (DataStoreLoad data)
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/" + data.profileText[0].text + ".bin";
+        string path = Application.persistentDataPath + "/Profile.bin";
         FileStream stream = new FileStream(path, FileMode.Create);
 
         ProfileData profiledata = new ProfileData(data);
@@ -15,6 +15,26 @@ public static class SaveLoad
         formatter.Serialize(stream, profiledata);
         stream.Close();
         
+    }
+
+    public static ProfileData LoadData()
+    {
+        string path = Application.persistentDataPath + "/Profile.bin";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            ProfileData data = formatter.Deserialize(stream) as ProfileData;
+
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Profile not found in " + path);
+            return null;
+        }
     }
 
 }
