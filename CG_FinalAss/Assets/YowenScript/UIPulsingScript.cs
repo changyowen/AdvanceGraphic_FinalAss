23b43993e6a2e2d018b4ef2fcbced26a5e4f03fb
changyowen <changyowen@gmail.com>
2020-10-01 01:56:00 +0800

    Update Fix bug

--- CG_FinalAss/Assets/YowenScript/UIPulsingScript.cs
+++ CG_FinalAss/Assets/YowenScript/UIPulsingScript.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class UIPulsingScript : MonoBehaviour
 {
-    private bool mouse_over = false;
     public Animator UIanim;
 
     // Start is called before the first frame update
     void Start()
     {
         UIanim = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        
     }
 
     public void MagnifyingFunction()
