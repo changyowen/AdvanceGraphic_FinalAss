5333ff43da7be8eaf4c8fffbf3b27b394494b233
changyowen <changyowen@gmail.com>
2020-09-30 12:50:11 +0800

    Update ScrollBar & Edit Scene Arrangement

---
+++ CG_FinalAss/Assets/YowenScript/UIPulsingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPulsingScript : MonoBehaviour
+{
+    private bool mouse_over = false;
+    public Animator UIanim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIanim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MagnifyingFunction()
+    {
+        UIanim.SetBool("Magnifying", true);
+    }
+
+    public void NormalFunction()
+    {
+        UIanim.SetBool("Magnifying", false);
+    }
+}
