06300b7294a10683c80213904e1202fd8f833796
changyowen <changyowen@gmail.com>
2020-10-01 13:55:23 +0800

    Update Edit Scene

--- CG_FinalAss/Assets/YowenScript/RefreshText.cs
+++ CG_FinalAss/Assets/YowenScript/RefreshText.cs
@@ -11,6 +11,7 @@ public class RefreshText : MonoBehaviour
 
     [Header("White Part")]
     public Text[] text_WhitePart;
+    public GameObject Skill_anchorPoint, workingExperience_obj, personalSkill_obj, workingButton, skillButton;
 
     void OnEnable()
     {
@@ -53,5 +54,26 @@ public class RefreshText : MonoBehaviour
                 text_WhitePart[i].color = Color.black;
             }
         }
+
+        RefreshPosition();
+    }
+
+    void RefreshPosition()
+    {
+        RectTransform anchorRect = Skill_anchorPoint.GetComponent<RectTransform>();
+        RectTransform WE_objRect = workingExperience_obj.GetComponent<RectTransform>();
+        RectTransform PS_objRect = personalSkill_obj.GetComponent<RectTransform>();
+        RectTransform workingButtonRect = workingButton.GetComponent<RectTransform>();
+        RectTransform skillButtonRect = skillButton.GetComponent<RectTransform>();
+
+        Vector2 newTextSize = new Vector2(WE_objRect.rect.width, text_WhitePart[11].preferredHeight);
+        WE_objRect.sizeDelta = newTextSize;
+        workingButtonRect.sizeDelta = newTextSize;
+        newTextSize = new Vector2(PS_objRect.rect.width, text_WhitePart[12].preferredHeight);
+        PS_objRect.sizeDelta = newTextSize;
+        skillButtonRect.sizeDelta = newTextSize;
+
+        Vector3 newPosition = new Vector3(anchorRect.anchoredPosition.x, WE_objRect.rect.height * -1);
+        anchorRect.anchoredPosition = newPosition;
     }
 }
